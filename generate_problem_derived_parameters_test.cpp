853e9765ac4b454157f24ab7ae0fdde4b79e1d2b
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-05 21:42:30 +0800

    [master] corrected test name.

--- generate_problem_derived_parameters_test.cpp
+++ generate_problem_derived_parameters_test.cpp
@@ -6,7 +6,7 @@ TEST(GenerateProblemDerivedParametersTest, GetSubcarrierSpacingTest) {
   EXPECT_EQ(GetSubcarrierSpacing(3), 120);
 }
 
-TEST(GenerateProblemDerivedParametersTest, GetSubchannelBandwidth) {
+TEST(GenerateProblemDerivedParametersTest, GetSubchannelBandwidthTest) {
   EXPECT_EQ(GetSubchannelBandwidth(60), 720000);
   EXPECT_EQ(GetSubchannelBandwidth(15), 180000);
 }
