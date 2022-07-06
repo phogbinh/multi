2420894550dfb01f5a12e38711fb02210e0c4e29
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-06 21:24:35 +0800

    [master] added generate problem derived parameters.

---
+++ generate_problem_derived_parameters.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include "Parser.h" // Parser, Parameters
+#include "generate_problem_derived_parameters.h"
+
+using namespace std;
+
+int main() {
+  freopen("problem_derived_parameters.txt", "w", stdout);
+  Parser parser;
+  Parameters parameters;
+  parser.GetParameters(parameters);
+  int subcarrierSpacing = GetSubcarrierSpacing(parameters.NUMEROLOGY);
+  printf("SUBCARRIER_SPACING=%d kHz\n", subcarrierSpacing);
+  printf("SUBCHANNEL_BANDWIDTH=%d Hz\n", GetSubchannelBandwidth(subcarrierSpacing));
+  double timeSlotDuration = GetTimeSlotDuration(parameters.NUMEROLOGY);
+  printf("TIME_SLOT_DURATION=%f s\n", timeSlotDuration);
+  printf("TIME_MINISLOT_DURATION=%f s", GetTimeMinislotDuration(timeSlotDuration, parameters.TIME_MINISLOTS_NUM));
+  return 0;
+}
---
+++ initialize.sh
@@ -0,0 +1,3 @@
+rm -r ./bin
+mkdir ./bin
+g++ generate_problem_derived_parameters.cpp -o ./bin/generate_problem_derived_parameters && ./bin/generate_problem_derived_parameters
