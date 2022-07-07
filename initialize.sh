1d4292ea61fcb223b0f463dc96a72143146cb520
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-07 19:17:48 +0800

    [master] viewed generated problem derived parameters on initialize.

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,4 @@
 rm -r ./bin
 mkdir ./bin
 g++ generate_problem_derived_parameters.cpp -o ./bin/generate_problem_derived_parameters && ./bin/generate_problem_derived_parameters
+cat ./problem_derived_parameters.txt
