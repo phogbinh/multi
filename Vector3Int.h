85773490fe76f1fe0fa8924f248bc9f3ce6f91a6
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-07 20:48:40 +0800

    [master] added default constructor, copy constructor, = for vec3int.

--- Vector3Int.h
+++ Vector3Int.h
@@ -1,7 +1,23 @@
+#ifndef VECTOR_3_INT_H
+#define VECTOR_3_INT_H
 class Vector3Int {
 public:
+  Vector3Int() {
+    // body intentionally empty
+  }
+  Vector3Int(const Vector3Int& vector) : Vector3Int(vector._x, vector._y, vector._z) {
+    // body intentionally empty
+  }
   Vector3Int(int x, int y, int z) : _x(x), _y(y), _z(z) {
     // body intentionally empty
+  }
+  Vector3Int& operator=(const Vector3Int& vector) {
+    if (this != &vector) {
+      _x = vector._x;
+      _y = vector._y;
+      _z = vector._z;
+    }
+    return *this;
   }
   const int GetX() const {
     return _x;
@@ -17,3 +33,4 @@ private:
   int _y = 0;
   int _z = 0;
 };
+#endif
