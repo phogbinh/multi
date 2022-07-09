51ad350f5df06df433d339ba5157a5194a1949d2
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-09 20:17:37 +0800

    [master] corrected handle folder users_positions.
    bug case: users_positions 目錄剛開始不存在時，這裡的 && mkdir 會被 short-circuit.

--- initialize.sh
+++ initialize.sh
@@ -4,7 +4,8 @@ g++ generate_problem_derived_parameters.cpp -o ./bin/generate_problem_derived_pa
 cat ./problem_derived_parameters.txt
 g++ generate_base_stations_positions.cpp -o ./bin/generate_base_stations_positions && ./bin/generate_base_stations_positions
 cat ./base_stations_positions.txt
-rm -r ./users_positions && mkdir ./users_positions
+rm -r ./users_positions
+mkdir ./users_positions
 g++ generate_users_begin_end_positions.cpp -o ./bin/generate_users_begin_end_positions && ./bin/generate_users_begin_end_positions
 ls ./users_positions
 $MATLAB_DIR/matlab -batch "run ./ml/generate_users_positions.m"
