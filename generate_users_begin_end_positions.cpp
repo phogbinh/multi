2dce7e7c24f26b0e20b8b976f623042a1919c84c
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-09 13:51:22 +0800

    [master] added generate users begin and positions.

---
+++ generate_users_begin_end_positions.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include "Parser.h"
+#include "UsersBeginEndPositionsGenerator.h"
+#include "Vector3Int.h"
+
+using namespace std;
+
+int main() {
+  Parser parser;
+  Parameters parameters;
+  parser.GetParameters(parameters);
+  UsersBeginEndPositionsGenerator generator(parameters.EMBB_USERS_NUM, parameters.URLLC_USERS_NUM, parameters.MINIMUM_X, parameters.MAXIMUM_X, parameters.MINIMUM_Y, parameters.MAXIMUM_Y, parameters.USER_Z);
+  auto generate_users_positions = [](const string filename, function<void(vector<Vector3Int>&)> GetPositions) {
+    ofstream file;
+    file.open(filename);
+    vector<Vector3Int> positions;
+    GetPositions(positions);
+    for (auto position : positions) {
+      file << position.GetX() << "," << position.GetY() << "," << position.GetZ() << endl;
+    }
+    file.close();
+  };
+  generate_users_positions("./users_positions/embb-0.txt", bind(&UsersBeginEndPositionsGenerator::GetEmbbUsersBeginPositions, generator, placeholders::_1));
+  generate_users_positions("./users_positions/embb-" + to_string(parameters.TIME_SLOTS_NUM - 1) + ".txt", bind(&UsersBeginEndPositionsGenerator::GetEmbbUsersEndPositions, generator, placeholders::_1));
+  generate_users_positions("./users_positions/urllc-0-0.txt", bind(&UsersBeginEndPositionsGenerator::GetUrllcUsersBeginPositions, generator, placeholders::_1));
+  generate_users_positions("./users_positions/urllc-" + to_string(parameters.TIME_SLOTS_NUM - 1) + "-" + to_string(parameters.TIME_MINISLOTS_NUM - 1) + ".txt", bind(&UsersBeginEndPositionsGenerator::GetUrllcUsersEndPositions, generator, placeholders::_1));
+  return 0;
+}
--- initialize.sh
+++ initialize.sh
@@ -4,3 +4,6 @@ g++ generate_problem_derived_parameters.cpp -o ./bin/generate_problem_derived_pa
 cat ./problem_derived_parameters.txt
 g++ generate_base_stations_positions.cpp -o ./bin/generate_base_stations_positions && ./bin/generate_base_stations_positions
 cat ./base_stations_positions.txt
+rm -r ./users_positions && mkdir ./users_positions
+g++ generate_users_begin_end_positions.cpp -o ./bin/generate_users_begin_end_positions && ./bin/generate_users_begin_end_positions
+ls ./users_positions
