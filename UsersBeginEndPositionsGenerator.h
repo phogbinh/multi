c34b2dac0b8d22d597f454e1e346ab5898cff38f
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-09 12:55:10 +0800

    [master] finished users begin end positions generator.

--- UsersBeginEndPositionsGenerator.h
+++ UsersBeginEndPositionsGenerator.h
@@ -2,7 +2,7 @@
 #define SEED_Y 2
 class UsersPositionsGenerator {
 public:
-  // output @ _embbUsersBeginPositions
+  // output @ _embbUsersBeginPositions, _embbUsersEndPositions, _urllcUsersBeginPositions, _urllcUsersEndPositions
   UsersPositionsGenerator(const int embbUsersNum, const int urllcUsersNum, const int minX, const int maxX, const int minY, const int maxY, const int z) {
     default_random_engine engineX(SEED_X);
     default_random_engine engineY(SEED_Y);
@@ -12,11 +12,38 @@ public:
     for (int i = 0; i < embbUsersNum; ++i) {
       _embbUsersBeginPositions[i] = Vector3Int(uniformX(engineX), uniformY(engineY), z);
     }
+    _embbUsersEndPositions.resize(embbUsersNum);
+    for (int i = 0; i < embbUsersNum; ++i) {
+      _embbUsersEndPositions[i] = Vector3Int(uniformX(engineX), uniformY(engineY), z);
+    }
+    _urllcUsersBeginPositions.resize(urllcUsersNum);
+    for (int i = 0; i < urllcUsersNum; ++i) {
+      _urllcUsersBeginPositions[i] = Vector3Int(uniformX(engineX), uniformY(engineY), z);
+    }
+    _urllcUsersEndPositions.resize(urllcUsersNum);
+    for (int i = 0; i < urllcUsersNum; ++i) {
+      _urllcUsersEndPositions[i] = Vector3Int(uniformX(engineX), uniformY(engineY), z);
+    }
   }
   // output @ positions
   void GetEmbbUsersBeginPositions(vector<Vector3Int>& positions) {
     positions = _embbUsersBeginPositions;
   }
+  // output @ positions
+  void GetEmbbUsersEndPositions(vector<Vector3Int>& positions) {
+    positions = _embbUsersEndPositions;
+  }
+  // output @ positions
+  void GetUrllcUsersBeginPositions(vector<Vector3Int>& positions) {
+    positions = _urllcUsersBeginPositions;
+  }
+  // output @ positions
+  void GetUrllcUsersEndPositions(vector<Vector3Int>& positions) {
+    positions = _urllcUsersEndPositions;
+  }
 private:
   vector<Vector3Int> _embbUsersBeginPositions;
+  vector<Vector3Int> _embbUsersEndPositions;
+  vector<Vector3Int> _urllcUsersBeginPositions;
+  vector<Vector3Int> _urllcUsersEndPositions;
 };
--- UsersBeginEndPositionsGenerator_test.cpp
+++ UsersBeginEndPositionsGenerator_test.cpp
@@ -20,3 +20,54 @@ TEST(UsersBeginEndPositionsGeneratorTest, GetEmbbUsersBeginPositionsTest) {
   EXPECT_EQ(positions[50].GetY(), 533);
   EXPECT_EQ(positions[50].GetZ(), 0);
 }
+
+TEST(UsersBeginEndPositionsGeneratorTest, GetEmbbUsersEndPositionsTest) {
+  Parser parser;
+  Parameters parameters;
+  parser.GetParameters(parameters);
+  UsersPositionsGenerator generator(parameters.EMBB_USERS_NUM, parameters.URLLC_USERS_NUM, parameters.MINIMUM_X, parameters.MAXIMUM_X, parameters.MINIMUM_Y, parameters.MAXIMUM_Y, parameters.USER_Z);
+  vector<Vector3Int> positions;
+  generator.GetEmbbUsersEndPositions(positions);
+  EXPECT_EQ(positions.size(), 100);
+  // seed of (x, y) is (1, 2)
+  EXPECT_EQ(positions[0].GetX(), 537);
+  EXPECT_EQ(positions[0].GetY(), 74);
+  EXPECT_EQ(positions[0].GetZ(), 0);
+  EXPECT_EQ(positions[50].GetX(), 464);
+  EXPECT_EQ(positions[50].GetY(), 928);
+  EXPECT_EQ(positions[50].GetZ(), 0);
+}
+
+TEST(UsersBeginEndPositionsGeneratorTest, GetUrllcUsersBeginPositionsTest) {
+  Parser parser;
+  Parameters parameters;
+  parser.GetParameters(parameters);
+  UsersPositionsGenerator generator(parameters.EMBB_USERS_NUM, parameters.URLLC_USERS_NUM, parameters.MINIMUM_X, parameters.MAXIMUM_X, parameters.MINIMUM_Y, parameters.MAXIMUM_Y, parameters.USER_Z);
+  vector<Vector3Int> positions;
+  generator.GetUrllcUsersBeginPositions(positions);
+  EXPECT_EQ(positions.size(), 10);
+  // seed of (x, y) is (1, 2)
+  EXPECT_EQ(positions[0].GetX(), 689);
+  EXPECT_EQ(positions[0].GetY(), 378);
+  EXPECT_EQ(positions[0].GetZ(), 0);
+  EXPECT_EQ(positions[6].GetX(), 537);
+  EXPECT_EQ(positions[6].GetY(), 74);
+  EXPECT_EQ(positions[6].GetZ(), 0);
+}
+
+TEST(UsersBeginEndPositionsGeneratorTest, GetUrllcUsersEndPositionsTest) {
+  Parser parser;
+  Parameters parameters;
+  parser.GetParameters(parameters);
+  UsersPositionsGenerator generator(parameters.EMBB_USERS_NUM, parameters.URLLC_USERS_NUM, parameters.MINIMUM_X, parameters.MAXIMUM_X, parameters.MINIMUM_Y, parameters.MAXIMUM_Y, parameters.USER_Z);
+  vector<Vector3Int> positions;
+  generator.GetUrllcUsersEndPositions(positions);
+  EXPECT_EQ(positions.size(), 10);
+  // seed of (x, y) is (1, 2)
+  EXPECT_EQ(positions[0].GetX(), 577);
+  EXPECT_EQ(positions[0].GetY(), 153);
+  EXPECT_EQ(positions[0].GetZ(), 0);
+  EXPECT_EQ(positions[6].GetX(), 801);
+  EXPECT_EQ(positions[6].GetY(), 602);
+  EXPECT_EQ(positions[6].GetZ(), 0);
+}
