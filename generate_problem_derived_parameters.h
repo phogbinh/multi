c6c55c273c89b699a43ff4d196c3ab578f45e300
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-09 21:02:06 +0800

    [master] commented units.

--- generate_problem_derived_parameters.h
+++ generate_problem_derived_parameters.h
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
 
+// input @ numerology
+// output @ [kHz]
 int GetSubcarrierSpacing(const int numerology) {
   return (int)pow(2.0, (double)numerology) * 15;
 }
 
+// input @ subcarrierSpacing[kHz]
+// output @ [Hz]
 int GetSubchannelBandwidth(const int subcarrierSpacing) {
   return 12 * subcarrierSpacing * (int)1E3;
 }
 
+// input @ numerology
+// output @ [s]
 double GetTimeSlotDuration(const int numerology) {
   return pow(2.0, -(double)numerology) * 1E-3;
 }
 
+// input @ timeSlotDuration[s], timeMinislotsNum
+// output @ [s]
 double GetTimeMinislotDuration(const double timeSlotDuration, const int timeMinislotsNum) {
   return timeSlotDuration / (double)timeMinislotsNum;
 }
