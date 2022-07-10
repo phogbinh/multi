cfbacf832b355e0cc8bc4c6a4922abb49083526b
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-10 10:03:31 +0800

    [master] added get path loss and shadowing.

--- generate_problem_derived_parameters.h
+++ generate_problem_derived_parameters.h
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
+#define _USE_MATH_DEFINES // M_PI
+#define MINIMUM_FREQUENCY 25000000 // [Hz]
+#define LIGHT_SPEED 3E8 // [m/s]
+#define PATH_LOSS_EXPONENT 2.0
+#define SHADOWING_STANDARD_DEVIATION 4.6 // [dB]
+
 // input @ numerology
 // output @ [kHz]
 int GetSubcarrierSpacing(const int numerology) {
@@ -28,4 +36,16 @@ double GetTimeMinislotDuration(const double timeSlotDuration, const int timeMini
 // output @ [Hz]
 long long int GetSubchannelFrequency(const long long int baseFrequency, const size_t subchannelIdx, const int subchannelBandwidth) {
   return baseFrequency + (long long int)subchannelIdx * (long long int)subchannelBandwidth;
+}
+
+// input @ frequency[Hz], distance[m]
+// output @ [dB]
+double GetPathLossShadowing(const long long int frequency, const double distance) {
+  if (frequency < MINIMUM_FREQUENCY) {
+    throw runtime_error("frequency is smaller than minimum frequency");
+  }
+  if (distance < 1.0) {
+    throw runtime_error("distance is smaller than 1");
+  }
+  return 20.0 * log10(4.0 * M_PI * (double)frequency / LIGHT_SPEED) + 10.0 * PATH_LOSS_EXPONENT * log10(distance) + SHADOWING_STANDARD_DEVIATION;
 }
--- test/generate_problem_derived_parameters_test.cpp
+++ test/generate_problem_derived_parameters_test.cpp
@@ -24,3 +24,9 @@ TEST(GenerateProblemDerivedParametersTest, GetSubchannelFrequencyTest) {
   EXPECT_EQ(GetSubchannelFrequency(28000000000, 0, 720000), 28000000000);
   EXPECT_EQ(GetSubchannelFrequency(28000000000, 50, 720000), 28036000000);
 }
+
+TEST(GenerateProblemDerivedParametersTest, GetPathLossShadowingTest) {
+  EXPECT_THROW(double pathLossShadowing = GetPathLossShadowing(28000000000, 0.9), std::runtime_error);
+  EXPECT_THROW(double pathLossShadowing = GetPathLossShadowing(23000000, 1.0), std::runtime_error);
+  EXPECT_NEAR(GetPathLossShadowing(28000000000, 10.0), 85.98493281, DELTA);
+}
