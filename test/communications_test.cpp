2dbfcf1772d7cabf349658eb4668005467d1eaae
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-10 11:55:28 +0800

    [master] renamed generate problem derived params to communications.

--- test/communications_test.cpp
+++ test/communications_test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "test.h"
-#include "../generate_problem_derived_parameters.h"
+#include "../communications.h"
 
 TEST(GenerateProblemDerivedParametersTest, GetSubcarrierSpacingTest) {
   EXPECT_EQ(GetSubcarrierSpacing(2), 60);
--- test/test_unit.sh
+++ test/test_unit.sh
@@ -3,7 +3,7 @@ rm -r ./build
 cmake -S ./ -B ./build
 cmake --build ./build
 cd ./build
-./generate_problem_derived_parameters_test
+./communications_test
 ./Parser_test
 ./Vector3Int_test
 ./generate_base_stations_positions_test
