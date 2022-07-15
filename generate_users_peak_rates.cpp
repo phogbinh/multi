848409418bf9edefec530575ef175a9b5f6a20c7
phongbinh-laptop <phongbinh2511@gmail.com>
2022-07-15 13:32:16 +0800

    [master] added generate urllc users' peak rates.

--- generate_users_peak_rates.cpp
+++ generate_users_peak_rates.cpp
@@ -30,5 +30,25 @@ int main() {
     }
     file.close();
   }
+  const double TIME_MINISLOT_DURATION = GetTimeMinislotDuration(TIME_SLOT_DURATION, parameters.TIME_MINISLOTS_NUM);
+  for (size_t timeSlotIdx = 0; timeSlotIdx < parameters.TIME_SLOTS_NUM; ++timeSlotIdx) {
+    for (size_t timeMinislotIdx = 0; timeMinislotIdx < parameters.TIME_MINISLOTS_NUM; ++timeMinislotIdx) {
+      vector<Vector3Int> positions;
+      GetPositions(GetUrllcUsersPositionsFilepath(timeSlotIdx, timeMinislotIdx), positions);
+      ofstream file;
+      file.open(string(getenv("MULTI_DIR")) + "users_peak_rates/urllc-" + to_string(timeSlotIdx) + "-" + to_string(timeMinislotIdx) + ".txt");
+      for (size_t urllcUserIdx = 0; urllcUserIdx < parameters.URLLC_USERS_NUM; ++urllcUserIdx) {
+        for (size_t baseStationIdx = 0; baseStationIdx < parameters.BASE_STATIONS_NUM; ++baseStationIdx) {
+          vector<long long int> subchannelFrequencies(parameters.SUBCHANNELS_NUM);
+          for (size_t subchannelIdx = 0; subchannelIdx < parameters.SUBCHANNELS_NUM; ++subchannelIdx) {
+            subchannelFrequencies[subchannelIdx] = GetSubchannelFrequency(parameters.BASE_FREQUENCY, subchannelIdx, SUBCHANNEL_BANDWIDTH);
+          }
+          const double DISTANCE = baseStationsPositions[baseStationIdx].GetDistanceTo(positions[urllcUserIdx]); // downlink direction
+          file << GetUrllcUserBaseStationTimeMinislotPeakRate(SUBCHANNEL_BANDWIDTH, parameters.TRANSMITTED_POWER, subchannelFrequencies, DISTANCE, SUBCHANNEL_THERMAL_NOISE, TIME_MINISLOT_DURATION, parameters.TIME_MINISLOTS_NUM) << endl;
+        }
+      }
+      file.close();
+    }
+  }
   return 0;
 }
